aa5fd4c98b3c25e8e7b6ca81bd56990997fbed47
Mandrindra <hei.mandrindra@gmail.com>
2025-10-05 23:53:13 +0300

    commit

--- src/main/java/NGL/Utilisateur.java
+++ src/main/java/NGL/Utilisateur.java
@@ -4,17 +4,15 @@ package NGL;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
-
 
 @Getter
 @Setter
 
-public class UtilisateurReel extends UtilisateurAnonyme{
+public class Utilisateur extends UtilisateurAnonyme{
     private String nom;
     private String prenom;
 
-    public  UtilisateurReel(String nom, String prenom, int id) {
+    public  Utilisateur(String nom, String prenom, int id) {
         super(id);
         this.nom = nom;
         this.prenom = prenom;
--- src/test/java/NglTest.java
+++ src/test/java/NglTest.java
@@ -1,6 +1,6 @@
 import NGL.*;
 
-import NGL.Publication
+
 import org.junit.jupiter.api.Test;
 import java.time.Instant;
 import java.time.LocalDate;
@@ -13,22 +13,20 @@ class GradeServiceTest {
 
          @Test
     void TestPublication(){
+             Message message1 = new Message("Messi", "thank you", LocalDate.of(2000, 10, 11),"Messi@gmail.com",true);
+             Message message2 = new Message(2, "Jirama",LocalDate.of(1980, 10, 11));
              Publication publication1 = new Publication(1, "Rabe");
-             Publication publication2 = new Publication(2, "Expression Ecrite");
+
 
     }
     @Test
     void ajoutDePlusieursMessagesAunePublication(){
-        Message message1 = new Message("Koto", "Miala RAJOELINA", LocalDate.of(1980, 10, 11, "");
+        Message message1 = new Message("Koto", "Miala RAJOELINA", LocalDate.of(1980, 10, 11),;
         Message message2 = new Message(2, "Jirama",LocalDate.of(1980, 10, 11);
 
-
-        Message note1 = new Message(etudiants, exam1, 10);
-        Message note2 = new Message(etudiants, exam2, 15);
-
         List<Message> messages = new ArrayList<>();
-        messages.add(note1);
-        messages.add(note2);
+        messages.add(message1);
+        messages.add(message2);
 
         UtilisateurAnonyme grade = new UtilisateurAnonyme(messages);
 
